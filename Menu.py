aa15cdb342915562b1aa8376b1f414d9c840b92a
Kevin Allen <101743883+AllenDK@users.noreply.github.com>
2023-12-06 07:23:03 -0800

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -3,7 +3,7 @@ from tkinter import W, Entry, StringVar, Tk, Frame, Label, Button, PhotoImage, t
 
 # Funciones personalizadas para cada botón
 def action_1():
-    subprocess.Popen(["python3", "Citas.py"])
+    subprocess.Popen(["python3", "Menu.py"])
     root.withdraw()
 
 def action_2():
@@ -11,7 +11,7 @@ def action_2():
     root.withdraw()
 
 def action_3():
-    subprocess.Popen(["python3", "Ventas.py"])
+    subprocess.Popen(["python3", "Cotizacion.py"])
     root.withdraw()
 
 def action_4():
@@ -39,65 +39,39 @@ root.resizable(False, False)
 frame_franja = Frame(root, width=1700, height=80, bg='red')
 frame_franja.pack()
 
-# Frame para los botones
-frame_botones = Frame(frame_franja, bg='red')
-frame_botones.place(x=100, y=30)
-
 # Crear el fondo de la franja gris
-frame_franja_gris = Frame(root, bg='#4D4D4D')
-frame_franja_gris.pack(fill='x')
+#frame_franja_gris = Frame(root, bg='#4D4D4D')
+#frame_franja_gris.pack(fill='x')
 
-# Etiqueta con el texto "REFACCIONES" sobre la franja gris
-label_refacciones = Label(frame_franja_gris, text="Menu", bg='#4D4D4D', fg='white', font=('Microsoft YaHei UI Light', 20, 'bold'))
-label_refacciones.pack(padx=20, pady=10, side="left")
+# Etiqueta de texto sobre la franja gris
+label_nombre = Label(frame_franja, text="Concesionaria Dokaebi's", bg='red', fg='black', font=('Arial', 45, 'bold'))
+label_nombre.place(relx=0.5, rely=0.5, anchor='center')
 
 # Crear el fondo de la 3ra franja
-frame_franja_gris_1 = Frame(root, width=1000, height=350, bg='gray')
+frame_franja_gris_1 = Frame(root, width=1507, height=687, bg='gray')
 frame_franja_gris_1.pack()
 
-# Crear el fondo de la 4ta franja
-frame_franja_gris_2 = Frame(root, bg='#4D4D4D')
-frame_franja_gris_2.pack(fill='both', expand=True)
-
-# Etiqueta con el texto "Cotizacion Rapita" sobre la franja gris
-label_Cotizacion = Label(frame_franja_gris_2, text="Cotizacion", bg='#4D4D4D', fg='white',
-                          font=('Microsoft YaHei UI Light', 20, 'bold'))
-label_Cotizacion.pack(side="top", pady=20, anchor="center")
-
 # Textos para cada botón
-button_texts = ["CITAS", "CATALOGO", "VENTAS", "REFACCIONES", "SEGURO", "CERRAR SESION"]
+button_texts = ["AUTOS NUEVOS", "CATALOGO", "COTIZACION", "REFACCIONES", "SEGURO", "CERRAR SESION"]
 
-# Crear cinco botones horizontales con acciones personalizadas
+# Agregar la imagen como fondo usando un Label
+image_label = Label(frame_franja_gris_1, width=1507, height=687, bg='red')
+image_label.place(x=-3, y=0)
+
+img = PhotoImage(file='Imagenes/Menu.png')
+image_label.img = img  # Mantén una referencia al objeto PhotoImage para evitar que sea eliminado por el recolector de basura
+image_label.config(image=img)
+
+# Crear los botones y posicionarlos sobre el Label
+buttons = []
+
 for i in range(6):
-    button = Button(frame_botones, text=button_texts[i], command=actions[i], bg='gray', fg='white')
-    button.pack(side="left", padx=20, pady=5)
-
-# Agregar etiquetas y cuadros de texto para la información
-labels = ["Nombre:", "Apellido:", "Teléfono:", "Email:", "Modelo:"]
-text_entries = []
-
-for i in range(5):
-    label = Label(frame_franja_gris_2, text=labels[i], bg='#4D4D4D', fg='white')
-    label.pack(side="left", padx=10, pady=5)
-
-    entry = Entry(frame_franja_gris_2, width=20)
-    entry.pack(side="left", padx=5, pady=5)
-
-    text_entries.append(entry)
-
-# Función para imprimir la información cuando se presiona el botón
-def print_info():
-    for i in range(5):
-        print(f"{labels[i]} {text_entries[i].get()}")
-
-# Botón para guardar la información
-save_button = Button(frame_franja_gris_2, text="Guardar Información", command=print_info, bg='gray', fg='white')
-save_button.pack(side="left", pady=10, padx=20)
+    button = Button(frame_franja_gris_1, text=button_texts[i], command=actions[i], bg='gray', fg='white')
+    button.place(x=10 + i * 150, y=10)  
+    button.lift()  
+    buttons.append(button)
 
 img1 = PhotoImage(file='Imagenes/Logo.png')
 Label(root, image=img1, bg='red').place(x=10, y=10)
-
-img = PhotoImage(file='Imagenes/Menu.png')
-Label(root, width=1507, height=349, image=img, bg='red').place(x=-1, y=135)
 
 root.mainloop()
